25dbbeb7ca7ff8e5956222b68138cba9855a84cc
Gojer <gojer@naver.com>
2025-06-07 12:29:09 -0400

    Medium Commit

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,18 @@
 'use client';
+import FurnitureSelector from "./components/FurnitureSelector";
+import ImageUploader from "./components/ImageUploader";
+import { Preview } from "./components/Preview";
+import RoomTemplateSelector from "./components/RoomTemplateSelector";
+import Scene from "./components/Scene";
 
 export default function Home() {
   return (
    <>
     <h1 className="text-3xl font-bold text-red-500">Hello, World !</h1>
+    <ImageUploader />
+    <RoomTemplateSelector />
+    <FurnitureSelector />
+    <Scene />
    </>
   );
 }
