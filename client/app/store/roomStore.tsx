a86003e5d2f07f4517f1ec02ab8c852f63329055
Gojer <gojer@naver.com>
2025-06-07 09:05:37 -0400

    Fixing + add templates to room store.

--- client/app/store/roomStore.tsx
+++ client/app/store/roomStore.tsx
@@ -1,13 +1,28 @@
 import { create } from 'zustand';
 
+type BackgroundType = 'uploaded' | 'template' | null;
+
 interface RoomState {
+  backgroundType: BackgroundType;
+  templateId: string | null;
   imageUrl: string | null;
   setImageUrl: (url: string) => void;
-  clearImageUrl: () => void;
+  setTemplateId: (id: string) => void;
+  setBackgroundType: (type: BackgroundType) => void;
+  clearRoomData: () => void;
 }
 
 export const useRoomStore = create<RoomState>((set) => ({
+  backgroundType: null,
+  templateId: null,
   imageUrl: null,
   setImageUrl: (url) => set({ imageUrl: url }),
-  clearImageUrl: () => set({ imageUrl: null }),
+  setTemplateId: (id) => set({ templateId: id }),
+  setBackgroundType: (type) => set({ backgroundType: type }),
+  clearRoomData: () =>
+    set({
+      imageUrl: null,
+      templateId: null,
+      backgroundType: null,
+    }),
 }));
