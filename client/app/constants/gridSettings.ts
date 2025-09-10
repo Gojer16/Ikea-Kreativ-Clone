11bde8984154a593365f182c915eadf8ed52d39a
Gojer <gojer@naver.com>
2025-09-10 17:36:54 -0400

    making comments

--- client/app/constants/gridSettings.ts
+++ client/app/constants/gridSettings.ts
@@ -1,2 +1,3 @@
 export const GRID_CELL_SIZE = 1; // Defines the size of each grid cell
 export const SNAP_THRESHOLD = 0.2; // Defines how close an object needs to be to a grid line to snap
+export const PLANE_BOUNDS_XZ = 6; // Objects clamped to [-6, 6] on X and Z
